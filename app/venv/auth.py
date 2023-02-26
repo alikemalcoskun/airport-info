10bc9f1b6aca0a550b9a5073b5d73ab5932b7173
itu-itis21-coskunal20 <coskunal20@itu.edu.tr>
2023-02-26 16:34:35 +0300

    Add config.py

--- app/venv/auth.py
+++ app/venv/auth.py
@@ -6,6 +6,9 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 import database
 from model import TokenData
+import config
+from dotenv import load_dotenv
+import os
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -20,9 +23,9 @@ class Hash:
 
 
 class Token:
-    SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-    ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    ALGORITHM = config.ALGORITHM
+    ACCESS_TOKEN_EXPIRE_MINUTES = config.ACCESS_TOKEN_EXPIRE_MINUTES
 
     def __init__(self, SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES):
         self.SECRET_KEY = SECRET_KEY
